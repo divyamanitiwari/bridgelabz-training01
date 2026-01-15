92c6c51267fe513e1c0b747d1f9df2ffe362261f
Divya <divyamanitiwari01@gmail.com>
2026-01-15 15:09:47 +0530

    first

--- first.js
+++ first.js
@@ -1,3 +1,22 @@
-// this is my first code
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isstudent);
 
-console.log("Hello, Bridgelabz!");
+//6 operators
+let a=10;
+let b=5;
+
+console.log("Add:",a+b);
+console.log("Add:",a-b);
+console.log("Add:",a*b);
+console.log("Add:",a/b);
+
+//condition (if-else)
+let loginAge=18;
+if(loginAge>=18){
+    console.log("Allowed to login")
+}    
+else {
+    console.log("Not Allowed")
+}
